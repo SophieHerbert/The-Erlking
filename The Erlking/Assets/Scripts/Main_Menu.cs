6169eb6820fe47879042abeed908dcc08083b4d2
SophieHerbert <sherb0906@gmail.com>
2024-05-24 14:05:18 +0800

    The Erlking #12
    - I fixed my main menu

--- The Erlking/Assets/Scripts/Main_Menu.cs
+++ The Erlking/Assets/Scripts/Main_Menu.cs
@@ -5,14 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
+    public GameObject optionspanel;
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("The Erlking Game");
+        SceneManager.LoadScene(1);
+        //Debug.Log("I am in play");
+    }
+
+    public void Options()
+    {
+        optionspanel.SetActive(true);
     }
 
     public void QuitGame()
     {
-        Application.Quit();
+        Application.Quit(); // why
+        //Debug.Log("I am in quit this is jyst for me poopy");
     }
 
 }
